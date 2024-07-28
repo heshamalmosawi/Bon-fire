c6e890b01a02bcb635922b6cb1b0134b20faf411
akhaled <akhaledlarus@gmail.com>
2024-07-28 12:47:26 +0300

    fix(migrations): fixed sqlite code, and ran formatter

--- backend/pkgs/storage/migrations/000001_create_user_table.down.sql
+++ backend/pkgs/storage/migrations/000001_create_user_table.down.sql
@@ -1,6 +1,7 @@
-DROP TABLE user;
--- DROP TABLE public.user_activity;
 DROP TABLE user_follower;
+
 DROP TABLE user_following;
 
 DROP TABLE user_notification;
+
+DROP TABLE user;
--- backend/pkgs/storage/migrations/000001_create_user_table.up.sql
+++ backend/pkgs/storage/migrations/000001_create_user_table.up.sql
@@ -1,53 +1,38 @@
 CREATE TABLE user (
-    user_id TEXT NOT NULL,
-    user_email TEXT NOT NULL,
-    user_fname TEXT NOT NULL,
-    user_lname TEXT NOT NULL,
-    user_dob TEXT NOT NULL,
-    user_avatar_path TEXT NOT NULL,
-    user_nickname TEXT NOT NULL,
-    user_about TEXT NOT NULL,
-    profile_exposure TEXT,
-    
-    PRIMARY KEY (user_id)
+  user_id TEXT NOT NULL,
+  user_email TEXT NOT NULL,
+  user_fname TEXT NOT NULL,
+  user_lname TEXT NOT NULL,
+  user_dob TEXT NOT NULL,
+  user_avatar_path TEXT NOT NULL,
+  user_nickname TEXT NOT NULL,
+  user_about TEXT NOT NULL,
+  profile_exposure TEXT NOT NULL,
+  PRIMARY KEY (user_id)
 );
-
--- CREATE TABLE public.user_activity (
---     user_id TEXT NOT NULL DEFAULT (lower(hex(randomblob(16)))),
---     post_id TEXT NOT NULL DEFAULT (lower(hex(randomblob(16)))),
---     listing_id TEXT NOT NULL DEFAULT (lower(hex(randomblob(16))),
---     PRIMARY KEY (listing_id),
---     FOREIGN KEY (post_id) REFERENCES post(post_id),
---     FOREIGN KEY (user_id) REFERENCES "user"(user_id)
--- );
 
 CREATE TABLE user_follower (
-    user_id TEXT NOT NULL DEFAULT (lower(hex(randomblob(16)))),
-    follower_id TEXT NOT NULL DEFAULT (lower(hex(randomblob(16))),
-    listing_id TEXT NOT NULL DEFAULT (lower(hex(randomblob(16))),
-    PRIMARY KEY (listing_id),
-    FOREIGN KEY (follower_id) REFERENCES "user" (user_id) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES "user" (user_id) ON UPDATE CASCADE ON DELETE CASCADE
+  user_id TEXT NOT NULL,
+  follower_id TEXT NOT NULL,
+  PRIMARY KEY (user_id, follower_id),
+  FOREIGN KEY (follower_id) REFERENCES user(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES user(user_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
 
 CREATE TABLE user_following (
-    user_id TEXT NOT NULL DEFAULT (hex(randomblob(16))),
-    following_id TEXT NOT NULL DEFAULT (hex(randomblob(16))),
-    listing_id TEXT NOT NULL DEFAULT (hex(randomblob(16)),
-    PRIMARY KEY (listing_id),
-    FOREIGN KEY (following_id) REFERENCES "user" (user_id) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES "user" (user_id) ON UPDATE CASCADE ON DELETE CASCADE
+  user_id TEXT NOT NULL,
+  following_id TEXT NOT NULL,
+  PRIMARY KEY (user_id, following_id),
+  FOREIGN KEY (following_id) REFERENCES user(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES user(user_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-
-CREATE TABLE
-  user_notification (
-    noti_id TEXT PRIMARY KEY,
-    reciever_id TEXT,
-    noti_type TEXT,
-    noti_content TEXT,
-    noti_time TEXT,
-    noti_status TEXT DEFAULT 'unread',
-    FOREIGN KEY (reciever_id) REFERENCES "user" (user_id) ON UPDATE CASCADE ON DELETE CASCADE
-  );
+CREATE TABLE user_notification (
+  noti_id TEXT PRIMARY KEY,
+  reciever_id TEXT,
+  noti_type TEXT,
+  noti_content TEXT,
+  noti_time TEXT,
+  noti_status TEXT DEFAULT 'unread',
+  FOREIGN KEY (reciever_id) REFERENCES user(user_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
