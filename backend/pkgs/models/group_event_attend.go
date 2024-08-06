8926842bbcc7593d2e9480524ffd56825e9f4d90
halzaki <hashemalzaki44@gmail.com>
2024-08-06 16:43:05 +0300

    Done with the group table

---
+++ backend/pkgs/models/group_event_attend.go
@@ -0,0 +1,67 @@
+package models
+
+import (
+	"bonfire/pkgs/utils"
+	"github.com/gofrs/uuid"
+)
+
+type GroupEventAttend struct {
+	AttendID      uuid.UUID `json:"attendance_id"`
+	EventID      uuid.UUID `json:"event_id"`
+	AttendeeID    uuid.UUID `json:"attendee_id"`
+	ResponseType   string    `json:"response_Type"`
+}
+
+func (ga *GroupEventAttend) Save() error {
+	if ga.AttendID == uuid.Nil {
+		uid, err := uuid.NewV4()
+		if err != nil {
+			return err
+		}
+		ga.AttendID = uid
+	}
+
+	columns := []string{"attendance_id","event_id","attendee_id","response_Type"}
+	values := []interface{}{ga.AttendID,ga.EventID,ga.AttendeeID,ga.ResponseType}
+
+	_, err := utils.Create("group", columns, values)
+	return err
+}
+
+func (ga *GroupEventAttend) Del() error {
+	condition := "attendance_id = ?"
+	_, err := utils.Delete("group", condition, ga.AttendID)
+	return err
+}
+
+func (ga *GroupEventAttend) Update() error {
+	updates := map[string]interface{}{
+		"attendance_id" : ga.AttendID,
+		"event_id" : ga.EventID,
+		"attendee_id" : ga.AttendeeID,
+		"response_Type" : ga.ResponseType,
+	}
+	condition := "attendance_id = ?"
+	_, err := utils.Update("group", updates, condition, ga.AttendID)
+	return err
+}
+
+func GetAttendeeByGroup(attendeeID string) (*GroupEventAttend, error) {
+	columns := []string{"attendance_id","event_id","attendee_id","response_Type"}
+	condition := "attendee_id = ?"
+	rows, err := utils.Read("group", columns, condition, attendeeID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var attendee GroupEventAttend
+	if rows.Next() {
+		err := rows.Scan(&attendee.AttendID, &attendee.EventID, &attendee.AttendeeID, &attendee.ResponseType)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &attendee, nil
+}
