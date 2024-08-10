f8614cdabfd5d5b4084ab8763598c34b40be8e7f
xlvk <fatima77614@gmail.com>
2024-08-10 15:30:47 +0300

    delete(comment): deleted the GetCommentByCommentID function from the 01.go file.

--- backend/pkgs/models/01.go
+++ backend/pkgs/models/01.go
@@ -6,29 +6,6 @@ import (
 	"bonfire/pkgs/utils"
 )
 
-// GetCommentByCommentID retrieves a comment by its ID.
-func GetCommentByCommentID(commentID uuid.UUID) (*Comment, error) {
-	columns := []string{"comment_id", "author_id", "post_id", "comment_content", "comment_likecount", "created_at"}
-	condition := "comment_id = ?"
-	rows, err := utils.Read("comment", columns, condition, commentID)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	if !rows.Next() {
-		return nil, nil
-	}
-
-	var comment Comment
-	err = rows.Scan(&comment.CommentID, &comment.AuthorID, &comment.PostID, &comment.CommentContent, &comment.CommentLikeCount, &comment.CreatedAt)
-	if err != nil {
-		return nil, err
-	}
-
-	return &comment, nil
-}
-
 // GetPostByPostD retrieves a post by its ID.
 func GetPostByPostID(postID uuid.UUID) (*PostModel, error) {
 	columns := []string{"post_id", "post_content", "post_image_path", "post_exposure", "group_id", "post_likecount", "created_at", "author_id"}
