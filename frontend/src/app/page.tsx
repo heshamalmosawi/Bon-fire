93bd1c76391a622e2bd6639fb1b1ea13649f1811
akhaled01 <akhaledlarus@gmail.com>
2024-08-14 18:41:18 +0300

    cleaned frontend for integration start

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,33 +1,5 @@
 "use client";
 
-import { HandleFileUpload } from "@/utils/handleFileUpload";
-import { useState } from "react";
-
 export default function Home() {
-  const [file, setFile] = useState<File>();
-
-  const handleFileChange = (event: any) => {
-    setFile(event.target.files[0]);
-  };
-
-  const handleSubmit = async (event: any) => {
-    event.preventDefault();
-
-    if (!file) return;
-
-    const imageURI = await HandleFileUpload(file);
-
-    if (!imageURI) {
-      console.log("NO IMAGE URI RETURNED");
-    } else {
-      console.log(imageURI);
-    }
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="file" onChange={handleFileChange} />
-      <button type="submit">Upload</button>
-    </form>
-  );
+  return <></>;
 }
