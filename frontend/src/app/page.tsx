3a37029d6001ec533efe4315de05b71268fdec71
akhaled01 <akhaledlarus@gmail.com>
2024-08-19 00:48:32 +0300

    feat(bg): darkened BG

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ import PostComponent from "@/components/desktop/PostComponent";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-start">
+    <div className="w-screen h-screen flex flex-col items-center justify-start bg-[#111]">
       <PostComponent />
     </div>
   );
