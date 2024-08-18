ba6e80bad064772e64fc252818e37c012d995b39
akhaled01 <akhaledlarus@gmail.com>
2024-08-19 00:44:48 +0300

    feat(PostProps): added firstname and lastname

--- frontend/src/lib/interfaces.ts
+++ frontend/src/lib/interfaces.ts
@@ -2,12 +2,14 @@
  * these are properties each post will have
  */
 interface PostProps {
-    id: string;
-    username: string;
-    avatarUrl: string;
-    creationDate: Date;
-    postTextContent: string;
-    postImageContentUrl: string;
-    postLikeNum: number;
-    postCommentNum: number;   
+  id: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  avatarUrl: string;
+  creationDate: Date;
+  postTextContent: string;
+  postImageContentUrl: string;
+  postLikeNum: number;
+  postCommentNum: number;
 }
