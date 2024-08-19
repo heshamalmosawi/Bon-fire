080bb447232e08a51d0b3987ab6baa8f8b78af32
nqasim <nooraw376@gmail.com>
2024-08-19 16:31:57 +0300

    basic ui

--- frontend/src/components/desktop/SignupForm.tsx
+++ frontend/src/components/desktop/SignupForm.tsx
@@ -1,10 +1,274 @@
-import React from "react";
+import React, { FC, useState, ChangeEvent } from "react";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { signupSchema, HandleSignupSubmission } from "@/lib/schemas/signupSchema";
+import { useToast } from "../ui/use-toast";
 
-const SignupForm = () => {
+const SignupForm: FC = () => {
+  const { toast } = useToast();
+  const [stage, setStage] = useState(1);
+  const [avatarPreview, setAvatarPreview] = useState<string | null>(null);
+  const form = useForm<z.infer<typeof signupSchema>>({
+    resolver: zodResolver(signupSchema),
+  });
+
+  const onSubmit = async (values: z.infer<typeof signupSchema>) => {
+    console.log("Submitting form with values:", values);
+    try {
+      await HandleSignupSubmission(values);
+      toast({
+        title: "Success!",
+        description: "Signup successful!",
+      });
+    } catch (error) {
+      console.error("Signup submission failed:", error);
+      toast({
+        title: "Error!",
+        description: "Check the logs for more",
+        variant: "destructive",
+      });
+    }
+  };
+
+  const nextStage = () => {
+    console.log("Proceeding to next stage");
+    setStage(2);
+  };
+
+  const prevStage = () => {
+    console.log("Going back to previous stage");
+    setStage(1);
+  };
+
+  const handleAvatarChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files ? event.target.files[0] : null;
+    if (file) {
+      form.setValue("avatar", file);
+      const reader = new FileReader();
+      reader.onload = (e) => setAvatarPreview(e.target?.result as string);
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
-    <div>
-      <h1 className="text-white">HIIIIIIIIII</h1>
-    </div>
+    <main className="w-screen h-screen flex items-center justify-center bg-neutral-950">
+      <div className="w-[50%] h-full flex flex-col items-center justify-center gap-7">
+        {stage === 1 && (
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="space-y-6 w-[80%] mx-auto"
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-white">Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="w-full bg-neutral-900 text-white border-0 rounded-md"
+                        type="email"
+                        placeholder="john.doe@example.com"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-white">Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="w-full bg-neutral-900 text-white border-0 rounded-md"
+                        type="password"
+                        placeholder="********"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-white">Confirm Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="w-full bg-neutral-900 text-white border-0 rounded-md"
+                        type="password"
+                        placeholder="********"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="w-full bg-blue-400 hover:bg-blue-800 rounded-md"
+                type="button"
+                onClick={nextStage}
+              >
+                Next
+              </Button>
+            </form>
+          </Form>
+        )}
+        {stage === 2 && (
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="space-y-6 w-[80%] mx-auto"
+            >
+              <FormItem>
+                <FormLabel className="text-white">Avatar</FormLabel>
+                <FormControl>
+                  <div>
+                    <input
+                      type="file"
+                      accept="image/*"
+                      onChange={handleAvatarChange}
+                      className="w-full bg-neutral-900 text-white border-0 rounded-md file:btn file:btn-bordered file:btn-accent"
+                    />
+                    {avatarPreview && (
+                      <img
+                        src={avatarPreview}
+                        alt="Avatar Preview"
+                        className="mt-4 w-24 h-24 rounded-full mx-auto"
+                      />
+                    )}
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+              <FormField
+                control={form.control}
+                name="firstName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-white">First Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="w-full bg-neutral-900 text-white border-0 rounded-md"
+                        placeholder="John"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-white">Last Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="w-full bg-neutral-900 text-white border-0 rounded-md"
+                        placeholder="Doe"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+             <FormField
+                control={form.control}
+                name="dateOfBirth"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-white">Date of Birth</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="w-full bg-neutral-900 text-white border-0 rounded-md"
+                        type="date"
+                        value={field.value instanceof Date ? field.value.toISOString().substring(0, 10) : field.value || ''}
+                        onChange={field.onChange}
+                        onBlur={field.onBlur}
+                        ref={field.ref}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="nickname"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-white">Nickname (Optional)</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="w-full bg-neutral-900 text-white border-0 rounded-md"
+                        placeholder="Nickname"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="about"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-white">About Me (Optional)</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="w-full bg-neutral-900 text-white border-0 rounded-md"
+                        placeholder="Tell us about yourself..."
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex justify-between">
+                <Button
+                  className="bg-gray-600 text-white rounded-md py-2 hover:bg-gray-800"
+                  type="button"
+                  onClick={prevStage}
+                >
+                  Back
+                </Button>
+                <Button
+                  className="bg-blue-400 text-white rounded-md py-2 hover:bg-blue-800"
+                  type="submit"
+                >
+                  Complete Sign Up
+                </Button>
+              </div>
+            </form>
+          </Form>
+        )}
+      </div>
+    </main>
   );
 };
 
--- frontend/src/lib/schemas/signupSchema.ts
+++ frontend/src/lib/schemas/signupSchema.ts
@@ -1,41 +1,56 @@
 import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { CheckMimeType } from "../utils";
-import { HandleFileUpload } from "../handleFileUpload";
-import { useForm } from "react-hook-form";
+import { CheckMimeType } from "../utils"; 
+import { HandleFileUpload } from '../handleFileUpload'; 
+import axios from "axios";
 
-const signupSchema = z
-  .object({
-    firstName: z.string(),
-    lastName: z.string(),
-    userName: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6),
-    confirmPassword: z.string().min(6),
-    avatar: z
-      .instanceof(File)
-      .refine((file) => CheckMimeType(file), "mime type of file unacceptable") // this ensures user uplaods correct file type only
-      .optional(),
-    isPrivate: z.string().default("public"),
-  })
-  .superRefine(({ confirmPassword, password }, ctx) => {
-    if (confirmPassword !== password) {
-      ctx.addIssue({
-        code: "custom",
-        message: "The passwords did not match",
-        path: ["confirmPassword"],
-      });
+export const signupSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  userName: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6),
+  confirmPassword: z.string().min(6),
+  nickname : z.string(),
+  about : z.string(),
+  avatar: z.instanceof(File).refine(file => CheckMimeType(file), {
+    message: "mime type of file unacceptable"
+  }).optional(),
+  dateOfBirth: z.string().transform((str) => new Date(str)),
+  isPrivate: z.string().default("public"),
+}).superRefine(({ confirmPassword, password }, ctx) => {
+  if (confirmPassword !== password) {
+    ctx.addIssue({
+      code: "custom",
+      message: "The passwords did not match",
+      path: ["confirmPassword"]
+    });
+  }
+});
+
+export const HandleSignupSubmission = async (values: z.infer<typeof signupSchema>) => {
+  try {
+    const avatarUrl = values.avatar ? await HandleFileUpload(values.avatar) : null;
+    // Convert date to ISO string for consistent backend handling
+    const payload = {
+      ...values,
+      dateOfBirth: values.dateOfBirth.toISOString(), 
+      avatar: avatarUrl,
+    };
+
+    console.log("Final Payload:", payload);
+
+    // Send the data to the backend
+    const res = await axios.post("http://localhost:8080/signup", payload, {
+      withCredentials: true, // Include credentials if necessary for your backend
+    });
+
+    if (res.status !== 201) {
+      throw new Error("Backend responded with status " + res.status);
     }
-  });
 
-const HandleSignupSubmission = async (
-  values: z.infer<typeof signupSchema>
-) => {
-  // check if there is an image, and then upload and take the url instead
-  const payload = {
-    avatar: values.avatar ? await HandleFileUpload(values.avatar) : "",
-    ...values,
-  };
-
-  //TODO: continue with the rest here
+    console.log('Signup successful:', res.data);
+  } catch (error) {
+    console.error("Signup Submission Error:", error);
+    throw error; // rethrow or handle as needed
+  }
 };
