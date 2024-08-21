5b22772c44900eff2369d7a0b22b52f06fb91aec
sayhusain <hishamalmosawii@gmail.com>
2024-08-21 13:12:56 +0300

    initialize

---
+++ frontend/src/app/profile/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import ProfileComponent from "@/components/desktop/ProfileComponent";
+
+
+const ProfilePage = () => {
+    return (
+        <div className="w-screen h-screen flex flex-col items-center justify-start bg-[#111]">
+            <ProfileComponent />
+        </div>
+    );
+}
+export default ProfilePage;
