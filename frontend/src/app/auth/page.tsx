a7f6df24377934b519d09346cf8b9b6d47ec075e
xlvk <fatima77614@gmail.com>
2024-08-21 13:04:20 +0300

    add(mobile_login): worked on the mobile login a lil.

--- frontend/src/app/auth/page.tsx
+++ frontend/src/app/auth/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import AuthDesktop from "@/components/desktop/AuthDesktop";
+import AuthMobile from "@/components/mobile/AuthMobile";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import React from "react";
 
 const Page = () => {
   const isDesktop = useMediaQuery("(min-width: 768px)");
 
-  return isDesktop ? <AuthDesktop /> : <></>;
+  return isDesktop ? <AuthDesktop /> : <AuthMobile />;
 };
 
 export default Page;
