db7132f3954eaaf70d0306efd3b58c53a657d117
sayhusain <hishamalmosawii@gmail.com>
2024-08-21 13:12:56 +0300

    initialize

---
+++ frontend/src/components/desktop/ProfileComponent.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ProfileComponent = () => {
+    return (
+        <main className="w-screen h-screen bg-neutral-950">
+            <h1 style={{ color: "red" }}>Profile Component</h1>
+        </main>
+    );
+}
+export default ProfileComponent;
