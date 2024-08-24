ca19253f16c8ef1dbb657e7d46f2bb59f5a696be
sayhusain <hishamalmosawii@gmail.com>
2024-08-25 00:43:55 +0300

    fix backend logic, handle if no profile id available in both front&back

--- frontend/src/app/profile/[[...id]]/page.tsx
+++ frontend/src/app/profile/[[...id]]/page.tsx
@@ -5,29 +5,44 @@ import ProfileComponent from "@/components/desktop/ProfileComponent";
 import PostComponent from "@/components/desktop/PostComponent";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Navbar from "@/components/desktop/Navbar";
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 const ProfilePage = () => {
   const [sessionUser, setSessionUser] = useState("");
   const [profile, setProfile] = useState<{ fname: string; lname:string; avatarUrl: string; bio: string; nickname: string; privacy: string}>({ fname: "", lname: "", avatarUrl: "", bio: "", nickname: "", privacy: ""});
 
   const pathname = usePathname();
-  console.log("ur path", pathname);
-  let u_id = pathname.split("/")[2];
-  if (!u_id){
-    // TODO: send request to backend endpoint to validate session, remove session if not validated
-    let session_id = document.cookie.split("session_id=")[1];
-    if (session_id){
-      // ...
-    }
-    return;
-  }
+  const [u_id, setU_id] = useState(pathname.split("/")[2]);
+  const router = useRouter();
+
+  useEffect(() => {
+    const authenticate = async () => {
+      const response = await fetch(`http://localhost:8080/authenticate`, {
+        method: 'POST',
+        credentials: 'include',
+      });
+      console.log(response.status);
+      if (response.status !== 200 && u_id === undefined) {
+        console.log(`Failed to authenticate user: ${response.status}`);
+        router.push('/auth');
+        return;
+      } else if (response.status === 200) { // if user is authenticated and u_id is defined in URL
+        const data = await response.json();
+        console.log("authentication data:", data.User.user_id);
+        setSessionUser(data.User.user_id);
+        if (u_id === undefined) {
+          setU_id(data.User.user_id);
+        }
+      }
+    };
+    authenticate();
+  }, [router]);
 
 
   useEffect(() => {
     const fetchProfile = async () => {
       // TODO: find a way to get the user id from the index page or threw the url
-      const response = await fetch(`http://localhost:8080/profile/${u_id}`, { credentials: 'include' }); // Replace 'e' with the actual user ID
+      const response = await fetch(`http://localhost:8080/profile/${u_id}`, { credentials: 'include' });
       console.log(response.status)
       // try {
       if (response.status === 200) {
@@ -53,7 +68,7 @@ const ProfilePage = () => {
     };
 
     fetchProfile();
-  }, []);
+  }, [u_id]);
   console.log("profile object:", profile);
 
   return (
