ec587d125180e957fb95c9bc7ec958ea4e682305
akhaled01 <akhaledlarus@gmail.com>
2024-08-26 17:56:53 +0300

    fix(feed): returned on error, and serve the author of every post

--- backend/pkgs/server/feed.go
+++ backend/pkgs/server/feed.go
@@ -14,18 +14,44 @@ func GetFeed(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println("Error authorizing user:", err)
 		w.WriteHeader(http.StatusUnauthorized)
+		return
 	}
 
 	posts, err := models.GetViewablePosts(session.User.UserID)
 	if err != nil {
 		log.Println("error getting user feed:", err)
 		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
 	fmt.Println(posts)
 
-	if err := utils.EncodeJSON(w, posts); err != nil {
+	var result []struct {
+		Post   models.PostModel `json:"post"`
+		Author models.UserModel `json:"author"`
+	}
+
+	// we send the posts, and its author too
+	for _, post := range posts {
+		author, err := models.GetUserByID(post.AuthorID)
+		if err != nil {
+			log.Println("error getting post for user:", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		result = append(result, struct {
+			Post   models.PostModel `json:"post"`
+			Author models.UserModel `json:"author"`
+		}{
+			Post:   post,
+			Author: *author,
+		})
+	}
+
+	if err := utils.EncodeJSON(w, result); err != nil {
 		log.Println("error encoding user feed:", err)
 		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 }
