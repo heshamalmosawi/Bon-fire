f2ca31130f1207ff331bc7afceb372ae6bd59f2a
xlvk <fatima77614@gmail.com>
2024-08-28 14:33:05 +0300

    add(people): added the fetch functions to display all the users and be able to follow them.

--- frontend/src/components/desktop/PostComponent.tsx
+++ frontend/src/components/desktop/PostComponent.tsx
@@ -33,55 +33,57 @@ const PostComponent = (props: PostProps) => {
               <h6 className="text-[#ffffff66]">| {getAgo(props.creationDate)}</h6>
             </div>
             <h6 className="text-[#ffffff66]">
-              {props.username ? `@${props.username}` : ""}
+              | {getAgo(new Date(props.creationDate).toISOString())}
             </h6>
           </div>
+          <h6 className="text-[#ffffff66]">
+            {props.username ? `@${props.username}` : ""}
+          </h6>
         </div>
-        <div
-          id="post-content"
-          className="w-full flex flex-col items-center justify-center gap-4"
-        >
-          <div id="post-text-content" className="text-white text-sm w-full">
-            {props.postTextContent}
-          </div>
-          {props.postImageContentUrl ? (
-            <Image
-              src={props.postImageContentUrl}
-              className="w-full h-[200px] rounded-lg"
-              alt="post image"
-              width={400}
-              height={400}
-            />
-          ) : null}
-        </div>
-        <div className="w-full h-[44px] flex items-center justify-around gap-4 border-b-[0.1px] border-b-[#3838386f] border-t-[0.1px] border-t-[#3838386f]">
-          <ToolTipWrapper text={`${props.postLikeNum} Likes`}>
-            <Heart
-              color="white"
-              className="cursor-pointer hover:stroke-red-600 duration-300"
-            />
-          </ToolTipWrapper>
-          <ToolTipWrapper text={`${props.postCommentNum} comments`}>
-            <MessageSquare
-              color="white"
-              className="cursor-pointer hover:stroke-amber-400 duration-300"
-            />
-          </ToolTipWrapper>
-          <ToolTipWrapper text="wanna share?">
-            <Forward
-              color="white"
-              className="cursor-pointer hover:stroke-green-500 duration-300"
-            />
-          </ToolTipWrapper>
-        </div>
-        <Input
-          className="w-full bg-transparent text-white border-[0.1px] border-[#3838386f]"
-          placeholder="Add a comment..."
-        />
       </div>
-
-      {/* Comment Dialog */}
-      <CommentDialog isOpen={isDialogOpen} onClose={closeDialog} post={props} />
+      <div
+        id="post-content"
+        className="w-full flex flex-col items-center justify-center gap-2"
+      >
+        <div id="post-text-content" className="text-white text-sm">
+          {props.postTextContent}
+        </div>
+        {props.postImageContentUrl ? (
+          <Image
+            src={props.postImageContentUrl}
+            className="w-full h-[200px] rounded-lg"
+            alt="post image"
+            width={400}
+            height={400}
+          />
+        ) : (
+          <></>
+        )}
+      </div>
+      <div className="w-full h-[44px] flex items-center justify-around gap-4 border-b-[0.1px] border-b-[#3838386f] border-t-[0.1px] border-t-[#3838386f]">
+        <ToolTipWrapper text={`${props.postLikeNum} Likes`}>
+          <Heart
+            color="white"
+            className="cursor-pointer hover:stroke-red-600 duration-300"
+          />
+        </ToolTipWrapper>
+        <ToolTipWrapper text={`${props.postCommentNum} comments`}>
+          <MessageSquare
+            color="white"
+            className="cursor-pointer hover:stroke-amber-400 duration-300"
+          />
+        </ToolTipWrapper>
+        <ToolTipWrapper text="wanna share?">
+          <Forward
+            color="white"
+            className="cursor-pointer hover:stroke-green-500 duration-300"
+          />
+        </ToolTipWrapper>
+      </div>
+      <Input
+        className="w-full bg-transparent text-white border-[0.1px] border-[#3838386f]"
+        placeholder="Add a comment..."
+      />
     </div>
   );
 };
