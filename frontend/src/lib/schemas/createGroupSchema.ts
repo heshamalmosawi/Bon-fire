0d1b612a0bf82e2c24a561a3d2499dd68d9179a1
akhaled01 <akhaledlarus@gmail.com>
2024-08-29 22:37:05 +0300

    fmt and lock

--- frontend/src/lib/schemas/createGroupSchema.ts
+++ frontend/src/lib/schemas/createGroupSchema.ts
@@ -6,33 +6,33 @@ export const groupCreateSchema = z.object({
   groupDescrip: z.string().min(1, "Group description is required"),
 });
 
-
 export const HandleCreateGroup = async (
-    values: z.infer<typeof groupCreateSchema>
-  ) => {
-    // Create the payload with the form values
-    const payload = {
+  values: z.infer<typeof groupCreateSchema>
+) => {
+  // Create the payload with the form values
+  const payload = {
     group_name: values.groupName,
     group_desc: values.groupDescrip,
-    };
-  
-    try {
-      // Make a POST request to your backend API
-      const response = await fetch("http://localhost:8080/group/create", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(payload), 
-        credentials: "include", // what sends the cookie 
-      });
-      // Check if the response is successful
-      if (response.status !== 200) {
-        throw new Error("backend responded with status: " + response.status.toString());
-      }
-  
-    } catch (error) {
-      console.error("Error creating group:", error);
-      throw error;
+  };
+
+  try {
+    // Make a POST request to your backend API
+    const response = await fetch("http://localhost:8080/group/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+      credentials: "include", // what sends the cookie
+    });
+    // Check if the response is successful
+    if (response.status !== 200) {
+      throw new Error(
+        "backend responded with status: " + response.status.toString()
+      );
     }
+  } catch (error) {
+    console.error("Error creating group:", error);
+    throw error;
+  }
 };
