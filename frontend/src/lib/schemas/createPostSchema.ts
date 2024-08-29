c60c5b21fa1073bd17589e40b24c87aaa582a98f
akhaled01 <akhaledlarus@gmail.com>
2024-08-30 00:13:47 +0300

    fix(create-post): fixed image upload and cleaned up func

--- frontend/src/lib/schemas/createPostSchema.ts
+++ frontend/src/lib/schemas/createPostSchema.ts
@@ -10,42 +10,23 @@ export const createPostSchema = z.object({
 export const HandleCreatePost = async (
   values: z.infer<typeof createPostSchema>
 ) => {
-  const payload = new FormData();
-
-  payload.append("post_content", values.textContent);
-  payload.append(
-    "post_image_path",
-    values.imageContent ? await HandleFileUpload(values.imageContent) : ""
-  );
-
-  const payload2 = {
+  const payload = {
     post_content: values.textContent,
-    post_image_path: values.imageContent ? await HandleFileUpload(values.imageContent) : "", // If you need to pass an image path
-    post_exposure: "public", // Replace with your actual value
-    group_id: "", // Optional, if you have a group ID
-};
+    post_image_path: values.imageContent
+      ? await HandleFileUpload(values.imageContent)
+      : "",
+    post_exposure: "public", // TODO: fix the exposure
+    group_id: "",
+  };
 
-
-  // export const creat_comment = async (comment_data) => {
-  //   const res = await fetch(BACKENDURL + "/comment/create", {
-  //     method: "POST",
-  //     body: JSON.stringify(comment_data),
-  //     credentials: "include",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   });
-  // };
-
-  console.log(payload2)
   const res = await fetch(`http://localhost:8080/post/create`, {
     method: "POST",
-    body: JSON.stringify(payload2),
+    body: JSON.stringify(payload),
     credentials: "include",
     headers: {
-        "Content-Type": "application/json",
+      "Content-Type": "application/json",
     },
-});
+  });
 
   if (res.status !== 200) {
     throw new Error("backend responded with status: " + res.status.toString());
