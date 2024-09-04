19ff897e6da173b43521a9fe97369235c2af0f5f
xlvk <fatima77614@gmail.com>
2024-09-05 01:37:36 +0300

    add(msgs): fixed the twice sending, the input is weird pls fares fix it.

--- frontend/src/components/desktop/Chat.tsx
+++ frontend/src/components/desktop/Chat.tsx
@@ -98,6 +98,7 @@ const Chat: React.FC<ChatProps> = ({ selectedUser, sessionUser }) => {
         chatContainer.removeEventListener("scroll", handleScroll);  // Cleanup event listener
       };
     }
+    setNewMessage("");
   }, [chatHistory]);  // Re-attach scroll listener if chat history changes
 
   const handleSendMessage = async () => {
@@ -236,7 +237,7 @@ const Chat: React.FC<ChatProps> = ({ selectedUser, sessionUser }) => {
                 //   </strong> {msg.message_content}
                 // </div>
               ))}
-              {handleMessages().map((msg, index) => (
+              {/* {handleMessages().map((msg, index) => (
                 <ChatBubble key={index} variant={msg.sender_id === sessionUser?.user_id ? "sent" : "received"}>
                   <ChatBubbleAvatar
                     src={msg.sender_id === sessionUser?.user_id ? sessionUser?.user_id : selectedUser.user_profile_pic || "https://img.freepik.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3467.jpg"}
@@ -247,7 +248,7 @@ const Chat: React.FC<ChatProps> = ({ selectedUser, sessionUser }) => {
                   </ChatBubbleMessage>
                   <ChatBubbleTimestamp timestamp={msg.message_timestamp?.toString() || new Date().toLocaleTimeString()} />
                 </ChatBubble>
-              ))}
+              ))} */}
             </>
           ) : (
             <div className="flex flex-col items-center justify-center h-full bg-black">
