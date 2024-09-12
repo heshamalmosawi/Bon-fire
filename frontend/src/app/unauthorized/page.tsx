1fed8b304342639651d89c13cc3fd336c360c1c1
xlvk <fatima77614@gmail.com>
2024-09-12 18:31:06 +0300

    fix(style_unauthorized): fixed the styleof the unauthorized page.

--- frontend/src/app/unauthorized/page.tsx
+++ frontend/src/app/unauthorized/page.tsx
@@ -1,8 +1,26 @@
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+
 const Unauthorized = () => {
+  const router = useRouter();
+
+  const handleGoBack = () => {
+    router.push("/groups");
+  };
+
     return (
-      <div style={{ padding: "20px", textAlign: "center" }}>
-        <h1>Access Denied</h1>
-        <p>You do not have permission to view this page.</p>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-neutral-950 text-white p-5">
+        <h1 className="text-4xl font-bold mb-4">Access Denied</h1>
+        <p className="text-lg mb-4">You do not have permission to view this page.</p>
+        <Button
+        onClick={handleGoBack}
+        // className="bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4 rounded"
+      >
+        Go Back to Groups Page
+      </Button>
       </div>
     );
   };
