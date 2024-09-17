6e344d442d4fdc9de36c2d00ef5cd7a842e2229e
akhaled01 <akhaledlarus@gmail.com>
2024-09-17 09:44:02 +0300

    feat(middleware): added a auth middleware in the frontend
    this allows for us to not serve any pages (other than `/auth`) unless a cookie is present

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const middleware = (request: NextRequest) => {
+    if (!request.cookies.get("session_id")) {
+        return NextResponse.redirect(new URL('/auth', request.url));
+    }
+}
+
+export const config = {
+    matcher: [
+        /*
+         * Apply middleware to all paths except:
+         * - /auth
+         * - Static files (/_next, /favicon.ico, etc.)
+         */
+        '/((?!auth|_next|favicon.ico|public).*)'
+    ]
+}
